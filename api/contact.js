1e687bf02f5ba9fcdb483b431455ba4981a82547
enescagl <enes.cagliyan@gmail.com>
2019-12-18 17:04:51 +0300

    this time followed instructions.

--- api/contact.js
+++ api/contact.js
@@ -1,6 +1,6 @@
 // with thanks to https://github.com/Urigo/graphql-modules/blob/8cb2fd7d9938a856f83e4eee2081384533771904/website/lambda/contact.js
 const sendMail = require('sendmail')()
-const { validateEmail, validateLength } = require('./validations')
+const { validateEmail, validateLength } = require('./contact/validations')
 
 exports.handler = (event, context, callback) => {
   if (!process.env.CONTACT_EMAIL) {
